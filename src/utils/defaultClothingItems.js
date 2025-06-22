83b37f2b9368f6fc9819a71f169f527bba1342e5
Fred Hobbs <FHobbs8030@Gmail.com>
2025-06-22 15:54:04 -0700

    add fallback clothing data

---
+++ src/utils/defaultClothingItems.js
@@ -0,0 +1,24 @@
+// src/utils/defaultClothingItems.js
+export const defaultClothingItems = [
+  {
+    _id: 0,
+    name: "Cap",
+    weather: "hot",
+    imageUrl:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png",
+  },
+  {
+    _id: 1,
+    name: "Hoodie",
+    weather: "warm",
+    imageUrl:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Hoodie.png",
+  },
+  {
+    _id: 2,
+    name: "Sneakers",
+    weather: "cold",
+    imageUrl:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sneakers.png",
+  },
+];
