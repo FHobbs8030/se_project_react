8d96835972d6b66f470e14bb53f98ee4139c2c24
Fred Hobbs <FHobbs8030@Gmail.com>
2025-06-22 08:40:16 -0700

    make a special function for fetching and checking responses

--- src/utils/clothingApi.js
+++ src/utils/clothingApi.js
@@ -1,37 +1,33 @@
 const isLocal = window.location.hostname === "localhost";
 const BASE_URL = isLocal ? "http://localhost:3001/clothingItems" : null;
 
+function checkResponse(res) {
+  if (!res.ok) {
+    throw new Error(`Server error: ${res.status}`);
+  }
+  return res.json();
+}
+
+function request(url, options) {
+  return fetch(url, options).then(checkResponse);
+}
+
 export const getClothingItems = () => {
-  return fetch(BASE_URL).then((res) => {
-    if (!res.ok) {
-      throw new Error(`Error fetching clothing items: ${res.status}`);
-    }
-    return res.json();
-  });
+  return request(BASE_URL);
 };
 
 export const addClothingItem = (item) => {
-  return fetch(BASE_URL, {
+  return request(BASE_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(item),
-  }).then((res) => {
-    if (!res.ok) {
-      throw new Error(`Error adding item: ${res.status}`);
-    }
-    return res.json();
   });
 };
 
 export const deleteClothingItem = (itemId) => {
-  return fetch(`${BASE_URL}/${itemId}`, {
+  return request(`${BASE_URL}/${itemId}`, {
     method: "DELETE",
-  }).then((res) => {
-    if (!res.ok) {
-      throw new Error(`Error deleting item: ${res.status}`);
-    }
-    return res.json();
   });
 };
