0024a28b3c3bf972159e3f62fef377e6e37a68a0
Fred Hobbs <FHobbs8030@Gmail.com>
2025-10-11 00:50:27 -0700

    modify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,6 @@ export default function Header({
 }) {
   const now = new Date();
   const formattedDate = new Intl.DateTimeFormat("en-US", {
-    weekday: "short",
     month: "short",
     day: "numeric",
   }).format(now);
