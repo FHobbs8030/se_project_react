090e708df9c80fd61c2f0208b8609e533cd43b7f
Fred Hobbs <FHobbs8030@gmail.com>
2025-12-16 21:01:23 -0800

    fix: render user avatar in Profile sidebar

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,43 @@
-import PropTypes from "prop-types";
-import ClothesSection from "./ClothesSection.jsx";
+import PropTypes from 'prop-types';
+import ClothesSection from './ClothesSection.jsx';
 
 export default function Profile({
   clothingItems,
   onCardClick,
   onAfterToggle,
-  currentUser
+  currentUser,
 }) {
   const uid = currentUser?._id || currentUser?.id || null;
+
   const ownItems = uid
-    ? clothingItems.filter((i) => {
+    ? clothingItems.filter(i => {
         const owner = i.owner;
         const oid =
-          typeof owner === "string" ? owner : owner?._id || owner?.id || null;
+          typeof owner === 'string' ? owner : owner?._id || owner?.id || null;
         return oid === uid;
       })
     : clothingItems;
 
+  const avatarSrc = currentUser?.avatar || '/images/Avatar.png';
+
   return (
     <main className="content">
-      <ClothesSection
-        clothingItems={ownItems}
-        onCardClick={onCardClick}
-        onAfterToggle={onAfterToggle}
-      />
+      <section className="profile">
+        <aside className="profile__sidebar">
+          <img
+            className="profile__avatar"
+            src={avatarSrc}
+            alt={currentUser?.name || 'User avatar'}
+          />
+          <p className="profile__name">{currentUser?.name}</p>
+        </aside>
+
+        <ClothesSection
+          clothingItems={ownItems}
+          onCardClick={onCardClick}
+          onAfterToggle={onAfterToggle}
+        />
+      </section>
     </main>
   );
 }
@@ -32,5 +46,5 @@ Profile.propTypes = {
   clothingItems: PropTypes.arrayOf(PropTypes.object).isRequired,
   onCardClick: PropTypes.func.isRequired,
   onAfterToggle: PropTypes.func,
-  currentUser: PropTypes.object
+  currentUser: PropTypes.object,
 };
